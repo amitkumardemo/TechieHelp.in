da29965941f2fdc0875ac693710e65fcde694f15
Amit Kumar <144897118+amitkumardemo@users.noreply.github.com>
2025-06-05 11:23:26 +0530

    Update MERN.jsx

--- src/components/MERN.jsx
+++ src/components/MERN.jsx
@@ -77,7 +77,7 @@ const MERN = () => {
         <p>
           Work on full-stack applications using MongoDB, Express.js, React.js, and Node.js. Gain hands-on experience with real projects and expert mentorship.
         </p>
-<a href="https://pages.razorpay.com/techiehelp" target="_blank" rel="noopener noreferrer">
+<a href="https://rzp.io/rzp/techiehelpInternship" target="_blank" rel="noopener noreferrer">
   <button className="mt-6 px-6 py-3 bg-cyan-600 text-white rounded-full hover:bg-cyan-700 transition duration-300">
     Apply Now
   </button>
