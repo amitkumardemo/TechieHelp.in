e95789f4043c825c49ca6c9a37931972d2089512
Amit Kumar <144897118+amitkumardemo@users.noreply.github.com>
2025-06-05 11:19:32 +0530

    Update Backend.jsx

--- src/components/Backend.jsx
+++ src/components/Backend.jsx
@@ -77,7 +77,7 @@ const Backend = () => {
         <p>
           Work with server-side logic, databases, and APIs using languages like Java, Python, or Node.js. Gain hands-on experience with real projects and expert mentorship.
         </p>
-<a href="https://pages.razorpay.com/techiehelp" target="_blank" rel="noopener noreferrer">
+<a href="https://rzp.io/rzp/techiehelpInternship" target="_blank" rel="noopener noreferrer">
   <button className="mt-6 px-6 py-3 bg-cyan-600 text-white rounded-full hover:bg-cyan-700 transition duration-300">
     Apply Now
   </button>
