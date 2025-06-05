57f53e935932b011fa3049652586fd204ebd439b
Amit Kumar <144897118+amitkumardemo@users.noreply.github.com>
2025-06-05 11:20:17 +0530

    Update DevOps.jsx

--- src/components/DevOps.jsx
+++ src/components/DevOps.jsx
@@ -77,7 +77,7 @@ const DevOps = () => {
         <p>
           Implement CI/CD pipelines, containerization with Docker, and deploy scalable apps with cloud platforms. Gain hands-on experience with real projects and expert mentorship.
         </p>
-        <a href="https://pages.razorpay.com/techiehelp" target="_blank" rel="noopener noreferrer">
+        <a href="https://rzp.io/rzp/techiehelpInternship" target="_blank" rel="noopener noreferrer">
   <button className="mt-6 px-6 py-3 bg-cyan-600 text-white rounded-full hover:bg-cyan-700 transition duration-300">
     Apply Now
   </button>
