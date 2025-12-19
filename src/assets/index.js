8882889e42a700377e6bd409a72160881d41c1bf
Kameshwar Patel <patelkameshwar01@gmail.com>
2025-12-19 16:09:04 +0530

    updated

--- src/assets/index.js
+++ src/assets/index.js
@@ -165,7 +165,7 @@ import puloviKChishi from "./Pulovi k Chishi.png";
 import kengimheingNampeung from "./kengimheing Nampeung.jpg";
 import kameshwarPatel from "./Kameshwar_patel.jpg";
 import ashok from "./Ashok.jpeg";
-import supriyaJalani from "./supriya_jalani.jpg";
+import supriyaJalani from "./supriya_jalani.jpeg";
 
 
 
